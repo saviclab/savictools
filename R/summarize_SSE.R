e1d98b794e48e7f94347cb94efdd5d36e2ffdb8c
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2021-03-31 14:56:17 -0700

    Made more general

--- R/summarize_SSE.R
+++ R/summarize_SSE.R
@@ -2,63 +2,72 @@
 #' @author Alexander Floren
 #'
 #' @description
-#' Parse a csv file of SSE summary statistics.
+#' `parse_sse()` parses a single .csv file of SSE summary statistics.
 #'
-#' To change the paramStats field, make sure you also change the corresponding
-#' param_rows field so that they match the row numbers in your sse_results.csv.
+#' @param path Path to the SSE results file.
+#' @param param_rows Row numbers of the desired parameter statistics in the
+#'   SSE results.
+#' @param n_theta Number of THETAs.
+#' @param n_omega Number of OMEGAs.
+#' @param n_sigma Number of SIGMAs.
+#' @param paramStats Parameter statistics to summarize. Must be present in
+#'   param_rows of the SSE results.
+#' @param type Type of error for ofv statistics. Either "I" or "II".
+#' @param ofv_cols Column numbers to include from the ofv statistics table.
+#' @param ofv_colNames Ofv statistics column names. Must be the same length as
+#'   ofv_cols.
+#' @param simNames Descriptive names for each alternative model, in the same
+#' order as in the SSE results.
 #'
-#' Choose type I or type II error by changing ofv to either "I" or "II".
-#'
-#' @param x
-#' @param param_rows
-#' @param param_colNames
-#' @param paramStats
-#' @param ofv
-#' @param ofv_cols
-#' @param ofv_colNames
-#' @param simNames
-#' @param summary
-#'
+#'   For example, if there are 3 alternative models called "alt_efv.mod",
+#'   "alt_wt.mod", and "alt_age.mod", in that order, then you could use
+#'   `simNames = c("efv", "wt, "age")` or something similar.
+#' @param summary If TRUE, parse_sse creates a folder called "summary" within
+#' the current directory and writes two .csv files to it summarizing the
+#' results.
 #' @usage
-#'
+#' `parse_sse(param_colNames, simNames)`
 #' @returns
-#'
+#'   A named list of dataframes.
+#'   "params" is a dataframe of parameter statistics.
+#'   "error" is a summary of ofv statistics (either type I or II).
 #' @examples
 #'
 #' @export
 
-# library(dplyr)
-# library(readr)
-# library(stringr)
 
-parse_sse <- function(x = "sse_results.csv",
+parse_sse <- function(path = "sse_results.csv",
                       param_rows = 19:20,
-                      param_colNames = c("metric", "temp", "TH_1", "TH_2",
-                                         "TH_3", "TH_4", "TH_5", "TH_6", "TH_7",
-                                         "TH_8", "TH_9", "OM_1", "OM_2", "OM_3",
-                                         "OM_4", "SI_1"),
+                      n_theta,
+                      n_omega,
+                      n_sigma,
                       paramStats = c("relative_rmse", "bias"),
-                      ofv = "II",
+                      type = "II",
                       ofv_cols = c(1, 4, 5, 6),
                       ofv_colNames = c("effect", "pow.05", "pow.01", "pow.001"),
-                      simNames = c("efv", "wt", "age"),
+                      simNames,
                       summary = FALSE) {
 
 
+  param_cols <- c("metric", "temp", paste0("TH_", 1:n_theta),
+                  paste0("OM_", 1:n_omega), paste0("SI_", 1:n_sigma))
+
   # read file
-  df <- readr::read_csv(x, col_names = param_colNames, skip_empty_rows = FALSE)
+  df <- suppressWarnings(
+    suppressMessages(
+      readr::read_csv(path, col_names = param_cols, skip_empty_rows = FALSE)))
 
   # check format
-  if (any(df[param_rows, param_colNames[1]] != paramStats)) {
+  if (any(df[param_rows, param_cols[1]] != paramStats, na.rm = TRUE)) {
     stop("Incorrect format. Check param_rows and param_stats.")
   }
 
   # find ofv stats
-  ofv_start <- which(df[, param_colNames[1]] == "ofv Statistics")
+  ofv_start <- which(df[, param_cols[1]] == "ofv Statistics")
   ofv_df <- df[ofv_start:nrow(df), ]
-  t1_start <- which(ofv_df[, param_colNames[1]] ==
+  t1_start <- which(ofv_df[, param_cols[1]] ==
                       "Type I error rate")
-  t2_start <- which(ofv_df[, param_colNames[1]] ==
+  t2_start <- which(ofv_df[, param_cols[1]] ==
                       "1 - type II error rate (power)")
 
   # format
@@ -71,7 +80,7 @@ parse_sse <- function(x = "sse_results.csv",
   t1_df <- t1_df[2:nrow(t1_df),]
   t2_df <- t2_df[2:nrow(t2_df),]
 
-  param_df <- df[param_rows, c(1, 3:length(param_colNames))]
+  param_df <- df[param_rows, c(1, 3:length(param_cols))]
 
   # output
   if (summary) {
@@ -79,16 +88,18 @@ parse_sse <- function(x = "sse_results.csv",
     readr::write_csv(param_df, "summary/param_stats.csv", append = TRUE)
 
 
-    if (ofv == "I") {
+    if (type == "I") {
       readr::write_csv(t1_df, "summary/type_I_error_ofv.csv", append = TRUE)
     }
-    else if (ofv == "II") {
+    else if (type == "II") {
       readr::write_csv(t2_df, "summary/type_II_error_ofv.csv", append = TRUE)
     }
   }
   error_df <- t2_df
-  if (ofv == "I") error_df <- t1_df
-  error_df[sym(ofv_colNames[1])] <- simNames
+  if (type == "I") {
+    error_df <- t1_df
+  }
+  error_df[dplyr::sym(ofv_colNames[1])] <- simNames
 
   list("params" = param_df, "error" = error_df)
 }
@@ -98,9 +109,10 @@ parse_sse <- function(x = "sse_results.csv",
 #'
 #' @description
 #' Recursively search a directory for SSE results, and combine into dataframes
-#' for parameter metrics and error.
+#' for parameter metrics and error rates.
 #'
 #' @usage
+#' `get_sse()`
 #'
 #' @returns a list of dataframes
 #'
@@ -108,14 +120,29 @@ parse_sse <- function(x = "sse_results.csv",
 #'
 #' @export
 
-get_sse <- function(parameter, induc = FALSE, ...) {
+get_sse <- function(path, exclude = NULL, ...) {
 
-  path_names <- list.files(pattern = "sse_results.csv", recursive = TRUE)
+  # convert to global path
+  path <- Sys.glob(path)
 
-  # remove induc results
-  if (!induc) {
-    path_names <- grep("induc", path_names, invert = TRUE, value = TRUE)
+  # remove trailing "/" character from path if present
+  if (substr(path, nchar(path), nchar(path)) == "/") {
+    path <- substr(path, 1, nchar(path) - 1)
   }
+
+  path_names <- list.files(path, pattern = "sse_results.csv", recursive = TRUE,
+                           full.names = TRUE)
+
+  # exclude certain files
+  if (!is.null(exclude)) {
+    path_names <- grep(exclude, path_names, invert = TRUE, value = TRUE)
+  }
+
+  # determine directory depth
+  relative_paths <- stringr::str_remove(path_names, path)
+
+  # subtract 1 so as not to include "sse_results.csv" as a column
+  max_depth <- max(sapply(strsplit(relative_paths, "/"), length)) - 1
 
   # parse all sse_results.csv files
   all_results <- lapply(path_names, parse_sse, ...)
@@ -125,18 +152,24 @@ get_sse <- function(parameter, induc = FALSE, ...) {
   error_dfs <- list()
 
   for (i in seq_along(all_results)) {
-    params <- dplyr::select(all_results[[i]]$params, metric, all_of(parameter))
+    params <- all_results[[i]]$params
     error  <- all_results[[i]]$error
 
+    params_ncol <- ncol(params)
+    error_ncol <- ncol(error)
+
     # split path name on slashes
-    substrings <- strsplit(path_names[i], "/")[[1]]
-    ss  <- stringr::str_extract(substrings[1], "\\d+")
-    des <- substrings[2]
-    iiv <- stringr::str_extract(substrings[3], "\\d+")
+    substrings <- strsplit(relative_paths[i], "/")[[1]]
 
+    # assign new column values
+    for (j in 1:max_depth) {
+      params[ , params_ncol + j] <- substrings[j]
+      error[ , error_ncol + j] <- substrings[j]
+    }
 
-    params <- dplyr::mutate(params, ss = ss, des = des, iiv = iiv)
-    error  <- dplyr::mutate(error, ss = ss, des = des, iiv = iiv)
+    # ss  <- stringr::str_extract(substrings[1], "\\d+")
+    # des <- substrings[2]
+    # iiv <- stringr::str_extract(substrings[3], "\\d+")
 
     param_dfs[[i]] <- as.data.frame(params)
     error_dfs[[i]] <- as.data.frame(error)
