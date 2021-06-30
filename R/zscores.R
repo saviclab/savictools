8145376d4d7bf7b0560c70b6dc781c779e3be9ad
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2021-06-30 08:54:45 -0700

    changed default missing values to NA

--- R/zscores.R
+++ R/zscores.R
@@ -7,9 +7,9 @@
 #' `igrowup_standard.R`, `igrowup_restricted.R`, and `who2007.R`.
 #'
 #' @param data A dataframe with ID, AGE, SEX, WT, and HT columns.
-#' @param missing_flag Value to replace NAs. Default is -99.
+#' @param missing_flag Value used to replace missing z-scores Default is NA.
 #' @param extreme_flag Value used to replace extreme/implausible z-scores.
-#' Default is -98.
+#' Default is NA.
 #'
 #' @importFrom magrittr %>%
 #' @export
@@ -18,7 +18,7 @@
 # TODO: Clean up WHO function docs
 # TODO: Make lines wrap at 80 characters
 
-zscores <- function(data, missing_flag=-99, extreme_flag=-98) {
+zscores <- function(data, missing_flag=NA, extreme_flag=NA) {
   start <- Sys.time()
   data$rownum <- 1:nrow(data)
 
