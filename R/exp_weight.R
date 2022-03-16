4eaf39f8e748abc45aa39861e24b763963a12269
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2022-03-16 13:50:45 -0700

    added exp-weight function

---
+++ R/exp_weight.R
@@ -0,0 +1,23 @@
+#' @title Calculate expected weight based on age and sex for under-5 children.
+#' @author Alexander Floren
+#'
+#' @description Calculate expected weight based on age and sex for under-5 children.
+#'
+#' @param age Age.
+#' @param sex Sex. 1 = male, 2 = female.
+#' @param units Units for age. Default is "months".
+#'
+#' @export
+exp_weight <- function(age, sex, units = c("months", "years", "weeks")) {
+  units <- match.arg(units)
+  # convert units to months, if necessary
+  if (units == "years") {
+    data$AGE <- data$AGE * 12
+  }
+  else if (units == "weeks") {
+    data$AGE <- data$AGE / 4.345
+  }
+
+  mapply(function(age, sex) {if (sex == 1) {expected_weight_table[age + 1, "EXP_WEIGHT_MALE"]}
+    else{expected_weight_table[age + 1, "EXP_WEIGHT_FEMALE"]}}, AGE, SEX)
+}
