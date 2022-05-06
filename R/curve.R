9fa6a1fb2f6ddc4ea29c1f6e01e06cf5b542d6d8
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2022-05-06 12:05:08 -0700

    remove col_name parameter, add ID check, docs

--- R/curve.R
+++ R/curve.R
@@ -1,15 +1,32 @@
-#' @title curve
+#' @title
+#' Detect individual PK curves
+#'
+#' @description
+#' `curve()` assigns a unique number to each PK curve in a dataset.
+#'
+#' @details
+#' A PK curve is defined as any sequence of consecutive 0s in the EVID column of
+#' length greater than or equal to 1, corresponding to the same ID.
+#'
+#' `curve()` is primarily used by `pk_plot()` in order to draw separate lines
+#' for distinct PK curves belonging to the same individual.
+#'
+#' @param data A dataframe with ID, and EVID columns.
+#'
 #' @export
 
-curve <- function(data, col_name = "CURVE") {
+curve <- function(data) {
   l <- nrow(data)
+  id <- data$ID
   evid <- data$EVID
   curves <- rep(0, l)
   in_curve <- FALSE
   curve_num <- 0
+  current_id <- id[1]
   for (i in 1:l) {
-    if (evid[i] != 0) {
+    if (evid[i] != 0 | id[i] != current_id) {
       in_curve <- FALSE
+      current_id <- id[i]
       next()
     }
     if (!in_curve) {
