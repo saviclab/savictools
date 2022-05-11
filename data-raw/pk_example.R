beccd4c2feb0811fc91f6059fbb6d64a26b5781c
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2022-05-11 10:19:03 -0700

    undo as_tibble

--- data-raw/pk_example.R
+++ data-raw/pk_example.R
@@ -1,2 +1,2 @@
-pk_example <- tibble::as_tibble(readr::read_csv("data-raw/pk_example.csv"))
+pk_example <- readr::read_csv("data-raw/pk_example.csv")
 usethis::use_data(pk_example, overwrite = TRUE)
--- data-raw/pop_example.R
+++ data-raw/pop_example.R
@@ -1,3 +1,3 @@
-pop_example <- tibble::as_tibble(readr::read_csv("data-raw/pop_example.csv"))
+pop_example <- readr::read_csv("data-raw/pop_example.csv")
 usethis::use_data(pop_example, overwrite = TRUE)
 
