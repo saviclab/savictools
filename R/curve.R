c6fdfe88d79febb8258bbf408e03032578e5368b
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2022-05-31 17:06:43 -0700

    make curve() use TAD

--- R/curve.R
+++ R/curve.R
@@ -20,23 +20,31 @@
 curve <- function(data) {
   data <- data %>%
     dplyr::arrange(ID, TIME, EVID)
+  if (!exists("TAD", data)) {
+    data <- tad(data)
+  }
   l <- nrow(data)
   id <- data$ID
   evid <- data$EVID
   curves <- rep(0, l)
+  tad <- data$TAD
+
   in_curve <- FALSE
   curve_num <- 0
   current_id <- id[1]
+  current_tad <- tad[1]
   for (i in 1:l) {
-    if (evid[i] != 0 | id[i] != current_id) {
+    if (evid[i] != 0 | id[i] != current_id | tad[i] < current_tad) {
       in_curve <- FALSE
       current_id <- id[i]
+      current_tad <- tad[i]
       next()
     }
     if (!in_curve) {
       in_curve <- TRUE
       curve_num <- curve_num + 1
     }
+    current_tad <- tad[i]
     curves[i] <- curve_num
 
   }
