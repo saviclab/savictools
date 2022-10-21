ec797972311b9cdb813f999d4416672b439c7507
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2022-10-21 08:58:03 -0700

    Adding cpp files to src/

--- R/tad.R
+++ R/tad.R
@@ -166,40 +166,41 @@ tad_old <- function(data, ...) {
     dplyr::arrange(ID, TIME, dplyr::desc(EVID))
 }
 
-Rcpp::cppFunction(
-  'NumericVector computeTAD(IntegerVector evid, NumericVector time, IntegerVector calc_tad) {
-
-  // assume data is already in expanded form (no ADDL)
-  bool dose_found = FALSE;
-  double last_dose = NA_REAL;
-
-  int length = evid.length();
-  NumericVector tad (length, NA_REAL);
-
-  for(int i = 0; i < length; i++) {
-
-        // only calculate TAD for rows satisfying cond
-        if(calc_tad[i] == 0) continue;
-
-        int this_evid = evid[i];
-        double this_time = time[i];
-
-        // if this is a dose record
-        if(this_evid == 1 || this_evid == 4) {
-
-          dose_found = TRUE;
-          tad[i] = 0;
-
-          last_dose = this_time;
-        }
-        else {
-
-          if(dose_found) {
-            tad[i] = this_time - last_dose;
-          }
-          // if no prior dosing record, leave TAD as NA.
-        }
-  }
-  return tad;
-}
-')
+#Rcpp::cppFunction(
+#  'NumericVector computeTAD(IntegerVector evid, NumericVector time, IntegerVector calc_tad) {
+#
+#  // assume data is already in expanded form (no ADDL)
+#  bool dose_found = FALSE;
+#  double last_dose = NA_REAL;
+#
+#  int length = evid.length();
+#  NumericVector tad (length, NA_REAL);
+#
+#  for(int i = 0; i < length; i++) {
+#
+#        // only calculate TAD for rows satisfying cond
+#        if(calc_tad[i] == 0) continue;
+#
+#        int this_evid = evid[i];
+#        double this_time = time[i];
+#
+#        // if this is a dose record
+#        if(this_evid == 1 || this_evid == 4) {
+#
+#          dose_found = TRUE;
+#          tad[i] = 0;
+#
+#          last_dose = this_time;
+#        }
+#        else {
+#
+#          if(dose_found) {
+#            tad[i] = this_time - last_dose;
+#          }
+#          // if no prior dosing record, leave TAD as NA.
+#        }
+#  }
+#  return tad;
+#}
+#')
+#
---
+++ src/computeTAD.cpp
@@ -0,0 +1,39 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+NumericVector computeTAD(IntegerVector evid, NumericVector time, IntegerVector calc_tad) {
+
+  // assume data is already in expanded form (no ADDL)
+  bool dose_found = FALSE;
+  double last_dose = NA_REAL;
+
+  int length = evid.size();
+  NumericVector tad (length, NA_REAL);
+
+  for(int i = 0; i < length; i++) {
+
+    // only calculate TAD for rows satisfying cond
+    if(calc_tad[i] == 0) continue;
+
+    int this_evid = evid[i];
+    double this_time = time[i];
+
+    // if this is a dose record
+    if(this_evid == 1 || this_evid == 4) {
+
+      dose_found = TRUE;
+      tad[i] = 0;
+
+      last_dose = this_time;
+    }
+    else {
+
+      if(dose_found) {
+        tad[i] = this_time - last_dose;
+      }
+      // if no prior dosing record, leave TAD as NA.
+    }
+  }
+  return tad;
+}
