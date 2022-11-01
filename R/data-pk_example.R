3066e912a16c039133765ab9b9ca76923df07b43
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2022-11-01 14:23:21 -0700

    document pk_example object

---
+++ R/data-pk_example.R
@@ -0,0 +1,17 @@
+#' PK dataset
+#'
+#' This is a NONMEM-formatted dataset intended for testing savictools functions.
+#'
+#' @format A tibble with 540 rows and 7 variables:
+#' \describe{
+#' \item{ID}{Patient ID}
+#' \item{TIME}{Event time}
+#' \item{AMT}{Dose amount}
+#' \item{DV}{Dependent value (concentration)}
+#' \item{EVID}{Event ID. 0 = observation, 1 = dose, 4 = reset-and-dose}
+#' \item{ADDL}{number of additional doses}
+#' \item{II}{Inter-dose interval}
+#' }
+#' @examples
+#' pk_example
+"pk_example"
--- data-raw/pk_example.R
+++ data-raw/pk_example.R
@@ -1,5 +1,5 @@
 #pk_example <- readr::read_csv("data-raw/pk_example.csv")
-pk_example <- data.frame(
+pk_example <- tibble(
   ID = rep(1:20, each = 27),
   TIME = rep(c(
     0, 24, 48, 72, 96, 120,
@@ -10,7 +10,7 @@ pk_example <- data.frame(
     2184,
     3143, 3863 # sparse samples
     ), 20),
-  AMT = c(
+  AMT = rep(c(
     rep(50, 6),
     rep(0, 8), # pk day 1
     50,
@@ -18,8 +18,8 @@ pk_example <- data.frame(
     rep(0, 8), # pk day 2
     50,
     0, 0 # sparse samples
-  ),
-  DV = c(
+  ), 20),
+  DV = rep(c(
     0, 0, 0, 0, 0, 0,
     1, 9, 10, 9, 8, 6, 4, 1, # pk day 1
     0,
@@ -27,8 +27,8 @@ pk_example <- data.frame(
     1, 9, 10, 9, 8, 6, 4, 1, # pk day 2
     0,
     1, 1 # sparse samples
-    ),
-  EVID = c(
+    ), 20),
+  EVID = rep(c(
     4, rep(1, 5),
     rep(0, 8), # pk day 1
     1,
@@ -36,7 +36,7 @@ pk_example <- data.frame(
     rep(0, 8), # pk day 2
     1,
     0, 0  # sparse samples
-    ),
+    ), 20),
   ADDL = rep(c(
     rep(0, 6),
     rep(0, 8), # pk day 1
