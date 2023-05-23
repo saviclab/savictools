5b2a0cce4e1998825053bfc2925c087afec61bc0
sandyfloren <62759566+sandyfloren@users.noreply.github.com>
2023-05-23 11:27:18 -0700

    add FFM adn BSA to expwt(), fix column selection bug

--- R/expwt.R
+++ R/expwt.R
@@ -5,6 +5,8 @@
 #' BMI-for-age z-score, if over 10 years of age) of 0 based on age and sex (and
 #' height, if over 10 years of age).
 #'
+#' Weight must be in kg, height in cm. Sex must be coded as: 1 = male, 2 = female.
+#'
 #' @returns A [tibble::tibble()] with an appended column, EXPWT, containing the
 #' expected weight,
 #'
@@ -21,7 +23,7 @@
 #' @export
 
 expwt <- function(data,
-                  units = c("months", "years", "weeks"),
+                  units = c("months", "years", "weeks", "days"),
                   age = grep("age",
                     colnames(data),
                     ignore.case = T,
@@ -32,39 +34,86 @@ expwt <- function(data,
                     ignore.case = T,
                     value = T
                   ),
+                  weight = grep("weight|wt",
+                                colnames(data),
+                                ignore.case = T,
+                                value = T
+                  ),
                   height = grep("height|ht",
                     colnames(data),
                     ignore.case = T,
                     value = T
                   )) {
   units <- match.arg(units)
-  age <- ifelse(identical(age, character(0)), NA, age)
-  sex <- ifelse(identical(sex, character(0)), NA, sex)
-  height <- ifelse(identical(height, character(0)), NA, height)
-  age_vec <- dplyr::pull(data, {{ age }})
+  age <- ifelse(identical(age, character(0)), NA, dplyr::pull(data, {{ age }}))
+  sex <- ifelse(identical(sex, character(0)), NA, dplyr::pull(data, {{ sex }}))
+  weight <- ifelse(identical(weight, character(0)), NA, dplyr::pull(data, {{ weight }}))
+  height <- ifelse(identical(height, character(0)), NA, dplyr::pull(data, {{ height }}))
 
-  # convert ager to months
+  # convert age to months
+  age_months <- age
   if (units == "years") {
-    age_vec <- age_vec * 12
+    age_months <- age * MONTHS_PER_YEAR
   } else if (units == "weeks") {
-    age_vec <- age_vec / 4.345
+    age_months <- age / WEEKS_PER_MONTH
+  } else if (units == "days") {
+    age_months <- age / DAYS_PER_YEAR
   }
+  age_days <- round(age_months * DAYS_PER_MONTH) # must be integer for EXPWT
+  age_years <- age_months * MONTHS_PER_YEAR
 
-  calc_expwt <- function(age, sex, height) {
-    age_days <- round(age * 30.4375)
-    if (age < 120) {
-      if (age_days <= 1856) {
-        savictools:::weianthro[which(savictools:::weianthro$sex == sex &
-          savictools:::weianthro$age == age_days), "m"]
-      } else {
-        savictools:::wfawho2007[which(savictools:::wfawho2007$sex == sex &
-          savictools:::wfawho2007$age == age), "m"]
-      }
+  data %>% dplyr::mutate(
+    EXPWT = mapply(calc_expwt, age_days, sex, height),
+    FFM = mapply(calc_ffm, age_years, sex, weight, height),
+    BSA = mapply(calc_bsa, eight, height)
+    )
+}
+
+DAYS_PER_YEAR <- 365.25
+MONTHS_PER_YEAR <- 12
+DAYS_PER_WEEK <- 7
+DAYS_PER_MONTH <- DAYS_PER_YEAR / MONTHS_PER_YEAR # 30.4375
+WEEKS_PER_YEAR <- DAYS_PER_YEAR / DAYS_PER_WEEK # 52.17857
+WEEKS_PER_MONTH <- WEEKS_PER_YEAR / MONTHS_PER_YEAR # 4.348214
+
+
+
+calc_expwt <- function(age, sex, height) {
+
+  # assume age in days
+  age_months <- round(age / DAYS_PER_MONTH)
+  # if age < 10 years
+  if (age < 10 * DAYS_PER_YEAR) {
+    if (age <= 1856) {
+      savictools:::weianthro[which(savictools:::weianthro$sex == sex &
+                                     savictools:::weianthro$age == age), "m"]
     } else {
-      bmi <- savictools:::bfawho2007[which(savictools:::bfawho2007$sex == sex &
-        savictools:::bfawho2007$age == age), ]$m
-      bmi * (height / 100)^2
+      savictools:::wfawho2007[which(savictools:::wfawho2007$sex == sex &
+                                      savictools:::wfawho2007$age == age_months), "m"]
     }
+  } else {
+    bmi <- savictools:::bfawho2007[which(savictools:::bfawho2007$sex == sex &
+                                           savictools:::bfawho2007$age == age_months), ]$m
+    bmi * (height / 100)^2
   }
-  data %>% dplyr::mutate(EXPWT = mapply(calc_expwt, !!rlang::sym(age), !!rlang::sym(sex), !!rlang::sym(height)))
+}
+
+
+calc_ffm <- function(age, sex, weight, height) {
+
+  bmi <- weight / ((height / 100)^2)
+  # assume age in years
+  ffm <- ifelse(sex == 1,
+         (0.88 + ((1 - 0.88) / (1 + (age / 13.4)^-12.7))) * ((9270 * weight) / (6680 + 216 * bmi)),
+         (1.11 + ((1 - 1.11) / (1 + (age / 7.1)^-1.1))) * ((9270 * weight) / (8780 + 244 * bmi))
+         )
+
+  ifelse(age < 3,
+         NA,
+         ffm)
+}
+
+calc_bsa <- function(weight, height) {
+  # Du Bois and Du Bois formula
+  0.007184 * (height^0.725) * (weight^0.425)
 }
